9d8726df788f34ff4760f19758b56da33864cf65
Muhammed Nazim <mnm9994u@gmail.com>
2013-03-24 16:21:20 +0100

    Add Expanded Desktop Tile [2/2]
    Change-Id: I2170ebe67adb54d0f05f0fb8da62f506b0fdc659
    
    Conflicts:
    	AndroidManifest.xml
    	src/com/android/settings/Settings.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -856,5 +856,6 @@ public class Settings extends PreferenceActivity
     public static class ASSDisplayRotationActivity extends Settings { /* empty */ }
     public static class ASSLockscreenActivity extends Settings { /* empty */ }
     public static class NotificationShortcutsSettingsActivity extends Settings { /* empty */ }
+    public static class PowerMenuSettingsActivity extends Settings { /* empty */ }
 
 }
